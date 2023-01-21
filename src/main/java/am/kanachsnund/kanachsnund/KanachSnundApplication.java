9a125ef567f0f878480616c6fd75f71d4a91c0d9
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2023-01-21 14:53:44 +0400

    init java project

---
+++ src/main/java/am/kanachsnund/kanachsnund/KanachSnundApplication.java
@@ -0,0 +1,13 @@
+package am.kanachsnund.kanachsnund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanachSnundApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KanachSnundApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/am/kanachsnund/kanachsnund/KanachSnundApplicationTests.java
@@ -0,0 +1,13 @@
+package am.kanachsnund.kanachsnund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanachSnundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
