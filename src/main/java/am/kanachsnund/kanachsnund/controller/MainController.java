0ddcc26e53043a361fc070cd62538c96a5af6d8e
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2023-01-21 15:11:02 +0400

    init java project

---
+++ src/main/java/am/kanachsnund/kanachsnund/controller/MainController.java
@@ -0,0 +1,13 @@
+package am.kanachsnund.kanachsnund.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String mainPage() {
+        return "pages/home";
+    }
+}
