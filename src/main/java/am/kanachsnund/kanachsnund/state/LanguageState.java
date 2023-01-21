f8b5f7b84f3ce17a053bfdc40bccaf2689b241e2
Hrayr Dilbaryan <hrair.dilbaryan@mail.ru>
2023-01-21 19:58:27 +0400

    sadfg

---
+++ src/main/java/am/kanachsnund/kanachsnund/state/LanguageState.java
@@ -0,0 +1,12 @@
+package am.kanachsnund.kanachsnund.state;
+
+public class LanguageState {
+
+    private String arm;
+    private String rus;
+    private String eng;
+
+    public LanguageState(String arm, String rus, String eng){
+
+    }
+}
