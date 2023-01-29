a0cb9c6325c7ca0ca5e4d77f26554972934a7e23
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2023-01-30 03:55:22 +0400

    commit

--- src/main/java/am/kanachsnund/kanachsnund/controller/advice/ExceptionHandlerAdvice.java
+++ src/main/java/am/kanachsnund/kanachsnund/controller/advice/ExceptionHandlerAdvice.java
@@ -3,28 +3,12 @@ package am.kanachsnund.kanachsnund.controller.advice;
 import am.kanachsnund.kanachsnund.Exception.EntityNotFoundException;
 import am.kanachsnund.kanachsnund.Exception.FileNotExistException;
 import am.kanachsnund.kanachsnund.Exception.UserNotFoundException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
-
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
-
-        return null;
-    }
 
     @ExceptionHandler(value = {
             EntityNotFoundException.class,
